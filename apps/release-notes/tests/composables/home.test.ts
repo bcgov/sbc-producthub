9a7416c71de2936cb7abeb383439d3a619a2f203
tronghieuvuong <tronghieu.vuong@gov.bc.ca>
2023-10-05 11:14:19 -0700

    Fix unit test for get labels.

---
+++ apps/release-notes/tests/composables/home.test.ts
@@ -0,0 +1,78 @@
+// const labelsRaw = [
+//   {
+//     node: {
+//       id: "Z2lkOi8vcmFwdG9yL0xhYmVsLzc0MzQ3OTEz",
+//       name: "Assets",
+//     },
+//   },
+//   {
+//     node: {
+//       id: "Z2lkOi8vcmFwdG9yL0xhYmVsLzgzOTc5NA",
+//       name: "bug",
+//     },
+//   },
+// ];
+
+// const labelExpected = [
+//   {
+//     id: "1",
+//     name: "Team 1",
+//   },
+//   {
+//     id: "2",
+//     name: "Team 2",
+//   },
+// ];
+
+import { getLabels } from '../../composable/classifyRelease';
+import { afterEach, describe, expect, it, vi } from "vitest";
+
+const labels = {
+  items: [
+    {
+      node: {
+        id: "1",
+        name: "Team 1",
+      },
+    },
+    {
+      node: {
+        id: "2",
+        name: "Team 2",
+      },
+    },
+  ],
+  expected:  [
+    {
+      id: "1",
+      name: "Team 1",
+    },
+    {
+      id: "2",
+      name: "Team 2",
+    },
+  ],
+  getLabels,
+};
+
+
+describe('helper functions test', () => {
+
+  afterEach(() => {
+    vi.resetAllMocks()
+  })
+
+  it('get labels withy a spy', () => {
+    const spy = vi.spyOn(labels, 'getLabels')
+    expect(spy.getMockName()).toEqual('getLabels')
+    expect(labels.getLabels(labels.items)).toEqual(
+      labels.expected
+    )
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    spy.mockImplementationOnce(() => []);
+    expect(labels.getLabels(labels.items)).toEqual([]);
+
+    expect(spy).toHaveBeenCalledTimes(2);
+  })
+})
