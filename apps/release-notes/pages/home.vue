64c93cf7487c258350ccde8a1de720a536e5d6b1
tronghieuvuong <tronghieu.vuong@gov.bc.ca>
2023-10-13 15:43:36 -0700

    Styling the page.

--- apps/release-notes/pages/home.vue
+++ apps/release-notes/pages/home.vue
@@ -37,40 +37,60 @@ export default {
 </script>
 
 <template>
-  <div class="release-page">
-    <h1>BC Registries Releases</h1>
-    <div v-if="releases">
-      <!-- Display the releases d ata here -->
-      <button class="state-button" @click="switchState">
-        {{ state }}
-      </button>
-      <ul>
-        <li v-for="release in display" :key="release.id">
-          <h1>{{ release.endOn }} - {{ release.state }}</h1>
-          <h2>Title {{ release.title }}</h2>
-          <div>Note {{ release.description }}</div>
-          <h2>Issues:</h2>
-          <ul>
-            <li v-for="issue in release.issues" :key="issue.id">
-              <NuxtLink :to="issue.htmlUrl">
-                {{ issue.number }}
-              </NuxtLink>
-              <h2>
-                {{ issue.title }} - {{ issue.number }} - {{ issue.htmlUrl }}
-              </h2>
-              <h3>Labels</h3>
+  <div>
+    <header>
+      <navbar />
+    </header>
+    <body>
+      <h1 class="title-page">
+        BC Registries Releases
+      </h1>
+      <div class="release-page">
+        <div v-if="releases">
+          <!-- Display the releases d ata here -->
+          <button class="state-button" @click="switchState">
+            {{ state }}
+          </button>
+          <div class="page">
+            <div class="date-range">
               <ul>
-                <li v-for="label in issue.labels" :key="label.id">
-                  <div>
-                    {{ label.name }}
-                  </div>
+                <li v-for="release in display" :key="release.id">
+                  {{ release.endOn }}
                 </li>
               </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+            </div>
+            <div class="content">
+              <ul>
+                <li v-for="release in display" :key="release.id">
+                  <h1>{{ release.endOn }} - {{ release.state }}</h1>
+                  <h1>Release {{ release.title }}</h1>
+                  <div>Note {{ release.description }}</div>
+                  <h2>Issues:</h2>
+                  <ul>
+                    <li v-for="issue in release.issues" :key="issue.id">
+                      <h2>
+                        {{ issue.title }} -
+                        <NuxtLink class="link" :to="issue.htmlUrl">
+                          {{ issue.number }}
+                        </NuxtLink>
+                      </h2>
+                      <!-- <h3>Labels</h3>
+                      <ul>
+                        <li v-for="label in issue.labels" :key="label.id">
+                          <div>
+                            {{ label.name }}
+                          </div>
+                        </li>
+                      </ul> -->
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
   </div>
 </template>
 
@@ -82,6 +102,7 @@ export default {
 h1 {
   font-size: 22px;
   font-weight: bolder;
+  color: #1669bb;
 }
 
 h2 {
@@ -95,9 +116,38 @@ h3 {
 }
 
 .state-button {
-  height: 30px;
-  width: 90px;
-  background-color: blue;
-  color: white;
+  width: 88px;
+  height: 34px;
+  border-radius: 4px;
+  background-color: #1669bb;
+  background-size: cover;
+  font-family: Noto Sans;
+  font-size: 14px;
+  color: #ffffff;
+  text-decoration: none solid rgb(255, 255, 255);
+  text-align: center;
+}
+
+.date-range {
+  flex-direction: column;
+  width: 20%;
+  font-weight: 500;
+}
+
+.title-page {
+  margin: 40px;
+}
+
+.page {
+  display: flex;
+}
+
+.content {
+  flex-direction: column;
+  width: 80%
+}
+
+.link {
+  color:#1669bb;
 }
 </style>
