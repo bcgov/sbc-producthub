25a9a427d9b2b88e38c2992570eb7cd6e4c287a6
tronghieuvuong <tronghieu.vuong@gov.bc.ca>
2023-11-21 12:06:22 -0800

    Metrics app

---
+++ apps/application-status/components/ButtonComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <button type="button" className="buttonClasses">
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style>
+.buttonClasses {
+  width: 88px ;
+  height: 34px;
+  border-radius: 4px;
+  background-color: #1669bb;
+  background-size: cover;
+  font-size: 14px;
+  color: #ffffff;
+  text-decoration: none solid rgb(255, 255, 255);
+  text-align: center;
+}
+</style>
---
+++ apps/application-status/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>
+        hello
+    </h1>
+    <h2>
+        {{ id }}
+    </h2>
+</template>
+
+<script lang="ts">
+ export default {
+    data(){
+        const id = 1
+        return {
+            id
+        }
+    }
+    
+ }
+</script>
+
