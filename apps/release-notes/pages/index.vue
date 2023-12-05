a1ff8e9d45efb5f28f5e5742a92a6ac793f4ee62
tronghieuvuong <tronghieu.vuong@gov.bc.ca>
2023-12-05 11:52:06 -0800

    Release notes: Fix content for legends and change text color.

--- apps/release-notes/pages/index.vue
+++ apps/release-notes/pages/index.vue
@@ -149,10 +149,12 @@ export default {
               <b class="title">
                 Release Dates
               </b>
-              <ul>
+              <ul >
                 <li v-for="release in display" :key="release.id">
                   <button @click="scrollToElement(release.id)">
-                    {{ release.endOn }}
+                    <p class="link underline underline-offset-1">
+                      {{ release.endOn }}
+                    </p>
                   </button>
                 </li>
               </ul>
