89bfb0fde2a935faf6faa21f9fa963ffe0844c21
tronghieuvuong <tronghieu.vuong@gov.bc.ca>
2023-12-06 12:15:01 -0800

    Fix linting errors.

--- apps/application-status/data/TeamsData.ts
+++ apps/application-status/data/TeamsData.ts
@@ -1,61 +1,61 @@
-import { Sprint } from "~/interface/interfaces";
-import BoardName from "~/enums/boardName";
-import BoardTitles from "~/enums/boardTitles";
+import { Sprint } from '~/interface/interfaces'
+import BoardName from '~/enums/boardName'
+import BoardTitles from '~/enums/boardTitles'
 const sprint: Sprint = {
   closedIssuesCount: 0,
   completedPoints: 0,
-  createdAt: "",
-  description: "",
-  endAt: "",
-  id: "",
-  name: "",
-  startAt: "",
-  state: "",
+  createdAt: '',
+  description: '',
+  endAt: '',
+  id: '',
+  name: '',
+  startAt: '',
+  state: '',
   totalPoints: 0,
-  updatedAt: "",
+  updatedAt: '',
   numberOfRelease: 0,
   issues: {
-    totalCount: 0,
-  },
-};
+    totalCount: 0
+  }
+}
 const result = {
   totalReleases: 0,
   totalIssues: 0,
-  totalBugs: 0,
-};
+  totalBugs: 0
+}
 const teams = [
   {
     id: 1,
     title: BoardTitles.ENTITIES,
     board: BoardName.ENTITIES,
-    keyWord: "enti",
+    keyWord: 'enti',
     result,
-    sprint,
+    sprint
   },
   {
     id: 2,
     title: BoardTitles.NAMESTEAMSPACE,
     board: BoardName.NAMETEAMSPACE,
-    keyWord: "name",
+    keyWord: 'name',
     result,
-    sprint,
+    sprint
   },
   {
     id: 3,
     title: BoardTitles.ASSETS,
     board: BoardName.ASSETS,
-    keyWord: "assets",
+    keyWord: 'assets',
     result,
-    sprint,
+    sprint
   },
   {
     id: 4,
     title: BoardTitles.RELATIONSHIPS,
     board: BoardName.RELATIONSHIPS,
-    keyWord: "relationships",
+    keyWord: 'relationships',
     result,
-    sprint,
-  },
-];
+    sprint
+  }
+]
 
 export default teams
